15b638c76ed6fdfb5f6cca31f33a4d5f1a408a48
Merri32 <Meronbihon13@gmail.com>
2025-06-30 00:20:49 +0300

    data

---
+++ virtual-art-gallery/src/data.js
@@ -0,0 +1,11 @@
+export const artworks = [
+  { texture: '/models/thumbnails/painting1.jpg', title: "Over Hills", artist: "Alemu Kebede", year: "2019" },
+  { texture: '/models/thumbnails/painting2.jpg', title: "Blue Reflections", artist: "Sara Tekle", year: "2020" },
+  { texture: '/models/thumbnails/painting3.jpg', title: "The Girl", artist: "Mikiyas Gebru", year: "2021" },
+  { texture: '/models/thumbnails/painting4.jpg', title: "Whispering Trees", artist: "Lidya Worku", year: "2018" },
+  { texture: '/models/thumbnails/painting5.jpg', title: "Harmony in Pink", artist: "Dereje Hailu", year: "2022" },
+  { texture: '/models/thumbnails/painting6.jpg', title: "Peaceful Path", artist: "Helen Mulu", year: "2023" },
+  { texture: '/models/thumbnails/painting7.jpg', title: "Golden Fields", artist: "Bekele Tesfaye", year: "2020" },
+  { texture: '/models/thumbnails/painting8.jpg', title: "City Lights", artist: "Selam Haile", year: "2021" },
+  { texture: '/models/thumbnails/painting9.jpg', title: "Royal", artist: "Nahom Desta", year: "2022" }
+];
